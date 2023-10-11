b7c4e8869fa11d4d0ab0d87abe1b8ceba08fb0e5
Iseason2000 <Iseason2000@qq.com>
2023-10-11 20:35:54 +0800

    修复玩家丢给怪物的物品会变异的bug

--- plugin/src/main/kotlin/top/iseason/bukkit/deenchantment/listeners/controllers/EntityDropItemListener.kt
+++ plugin/src/main/kotlin/top/iseason/bukkit/deenchantment/listeners/controllers/EntityDropItemListener.kt
@@ -1,5 +1,6 @@
 package top.iseason.bukkit.deenchantment.listeners.controllers
 
+import org.bukkit.entity.Mob
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -11,6 +12,18 @@ object EntityDropItemListener : Listener {
     fun onEntityDropItemEvent(event: EntityDropItemEvent) {
         val itemDrop = event.itemDrop
         val itemStack = itemDrop.itemStack
+        val entity = event.entity
+        if (entity is Mob) {
+            val equipment = entity.equipment
+            if (equipment != null) {
+                if (equipment.itemInMainHandDropChance == 1.0F) return
+                if (equipment.itemInOffHandDropChance == 1.0F) return
+                if (equipment.helmetDropChance == 1.0F) return
+                if (equipment.chestplateDropChance == 1.0F) return
+                if (equipment.leggingsDropChance == 1.0F) return
+                if (equipment.bootsDropChance == 1.0F) return
+            }
+        }
         EnchantTools.translateEnchantsByChance(itemStack)
     }
 }
